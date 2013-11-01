0dd756b2526d320cd893882fe841f022ad48c3bc
Stanley Yang <stanley.yang.sy@gmail.com>
2013-10-31 20:17:15 -0400

    oops uploaded an old version of the SQL before

--- Normalization 2.sql
+++ Normalization 2.sql
@@ -14,45 +14,114 @@ PRIMARY KEY (pid)
 );
 
 CREATE TABLE actors (
-pid INT REFERENCES people,
+aid INT REFERENCES people(pid),
 birthday DATE,
 hairColor VARCHAR(20),
 eyeColor VARCHAR(20),
 height INT,
 weight INT,
 guildAnniversary DATE,
-PRIMARY KEY (pid)
+PRIMARY KEY (aid)
 );
 
 CREATE TABLE directors (
-pid INT REFERENCES people,
-school VARCHAR(20),
+did INT REFERENCES people(pid),
+school VARCHAR(40),
 guildAnniversary DATE,
-PRIMARY KEY (pid)
+PRIMARY KEY (did)
 );
 
 CREATE TABLE movies (
 mid SERIAL,
-title VARCHAR(20),
+title VARCHAR(40),
 year INT,
-domesticSales INT,
-foreignSales INT,
-videoSales INT,
+domesticSales FLOAT4,
+foreignSales FLOAT4,
+videoSales FLOAT4,
 PRIMARY KEY (mid)
 );
 
 CREATE TABLE movieActors(
 mid INT REFERENCES movies,
-actor INT REFERENCES actors(pid),
-PRIMARY KEY (mid, actor)
+aid INT REFERENCES actors(aid),
+PRIMARY KEY (mid, aid)
 );
 
 CREATE TABLE movieDirectors(
 mid INT REFERENCES movies,
-director INT REFERENCES directors(pid),
-PRIMARY KEY (mid, director)
+did INT REFERENCES directors(did),
+PRIMARY KEY (mid, did)
 );
 
 INSERT INTO people (fname, lname, address)
 VALUES
-('Sean', 'Connery', '007 Bond St')
+('Sean', 'Connery', '007 Bond Drive'),
+('George', 'Lazenby', '008 James Rad'),
+('Roger', 'Moore', '009 Seven Street'),
+('Terence', 'Young', 'Thunder Ave'),
+('Guy', 'Hamilton', 'Live Path'),
+('Lewis', 'Gilbert', 'Twice Street'),
+('Peter', 'Hunt', 'Secret Street');
+
+INSERT INTO actors(aid, birthday, hairColor, eyeColor, height, weight, guildAnniversary)
+VALUES
+(1, '1930-08-25', 'Black', 'Brown', '74', '200', '1954-04-18'),
+(2, '1939-09-05', 'Black', 'Brown', '74', '180', '1965-09-17'),
+(3, '1930-10-14', 'Black', 'Brown', '73', '195', '1945-04-18');
+
+INSERT INTO directors(did, school, guildAnniversary)
+VALUES
+(4, 'University of Cambridge', '1940-04-10'),
+(5, 'None', '1952-06-30'),
+(6, 'None', '1966-07-04'),
+(7, 'University of Rome', '1940-07-18');
+
+INSERT INTO movies (title, year, domesticSales, foreignSales, videoSales)
+VALUES
+('Dr. No', 1962, 16.1, 43.5, 30.2),
+('From Russia with Love', 1963, 24.8, 54.1, 41.5),
+('Goldfinger', 1964, 51.1, 73.8, 79.1),
+('Thunderball', 1965, 63.6, 77.6, 92.1),
+('You Only Live Twice', 1967, 43.1, 68.5, 69.5),
+('On Her Majesty''Secret Service', 1969, 22.8, 59.2, 32.4),
+('Live and Let Die', 1973, 35.4, 91.0, 56.9);
+
+INSERT INTO movieActors (mid, aid)
+VALUES
+(1, 1),
+(2, 1),
+(3, 1),
+(4, 1),
+(5, 1),
+(6, 2),
+(7, 3);
+
+INSERT INTO movieDirectors (mid, did)
+VALUES
+(1, 4),
+(2, 4),
+(3, 5),
+(4, 4),
+(5, 6),
+(6, 7),
+(7, 5);
+
+SELECT * FROM people;
+SELECT * FROM actors;
+SELECT * FROM directors;
+SELECT * FROM movies;
+
+SELECT DISTINCT p.fname, p.lname
+FROM
+people p,
+movies m,
+directors d,
+movieDirectors md
+WHERE md.did = d.did AND m.mid = md.mid AND d.did = p.pid AND md.mid IN (SELECT m.mid
+									 FROM
+									 people p,
+									 actors a,
+									 movies m,
+									 movieActors ma
+									 WHERE p.pid = a.aid AND m.mid = ma.mid AND a.aid = ma.aid AND p.fname = 'Sean' AND p.lname = 'Connery'
+									 );
